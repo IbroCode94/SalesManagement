43c48b1d18166c727cd277769a16a62eb0b7891b
danieloboba <150180977+danieloboba@users.noreply.github.com>
2024-05-15 10:53:43 +0100

    commit

---
+++ src/main/java/org/sales/salesmanagement/AppConfig/OpenApiSwagger.java
@@ -0,0 +1,26 @@
+package org.sales.salesmanagement.AppConfig;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(
+        info = @Info(description = "Sales Management System"), security = {
+        @SecurityRequirement( name = "bearerAuth")
+}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiSwagger {
+
+}
